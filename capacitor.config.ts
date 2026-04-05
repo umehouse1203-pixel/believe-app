4842c2be84ef5b7c7bf296983d069e34ada058f1
HirokiUmezawa <hiroki.umezawa@jitera.com>
2026-04-05 21:30:55 +0900

    Add brand 'b' icon as favicon and apple-touch-icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.believe',
+  appId: 'com.believe.app',
   appName: 'believe',
   webDir: 'dist',
   server: {
